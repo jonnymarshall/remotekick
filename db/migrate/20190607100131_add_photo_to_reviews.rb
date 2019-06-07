0696f9cce5a53d37559092dd835f0fc0c1ef2d03
Jonny Marshall <jonnymarshall5@gmail.com>
2019-06-07 17:03:45 +0700

    migration bug fixed

---
+++ db/migrate/20190607100131_add_photo_to_reviews.rb
@@ -0,0 +1,5 @@
+class AddPhotoToReviews < ActiveRecord::Migration[5.2]
+  def change
+    add_column :reviews, :photo, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_093116) do
+ActiveRecord::Schema.define(version: 2019_06_07_100131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 2019_06_07_093116) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
