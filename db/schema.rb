1f59c1b0a7fcba669dff58cf1d65236b5d74d337
Jonny Marshall <jonnymarshall5@gmail.com>
2019-06-27 13:10:47 +0800

    created new model for opening hours

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_18_044953) do
+ActiveRecord::Schema.define(version: 2019_06_27_044310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 2019_06_18_044953) do
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_coffee_shops_on_user_id"
+  end
+
+  create_table "opening_hours", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "day"
+    t.time "open"
+    t.time "close"
   end
 
   create_table "review_photos", force: :cascade do |t|
