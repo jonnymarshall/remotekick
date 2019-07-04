f6e319a1d5ec365a9b5f6c503f73d791c6a5c2c4
Jonny Marshall <jonnymarshall5@gmail.com>
2019-07-04 18:07:49 +0800

    cleaned up js code

--- app/javascript/components/venue_autofill.js
+++ app/javascript/components/venue_autofill.js
@@ -2,6 +2,7 @@
 const venueAutofill = () => {
   console.log("hello from venue_autofill.js");
 
+  // Assign DOM elements to variables
   const searchBar = document.getElementById("venueNameInput");
   const dropdown = document.getElementById("myDropdown");
   const address = document.getElementById("address");
@@ -9,12 +10,35 @@ const venueAutofill = () => {
   const longitude = document.getElementById("longitude");
   const latitude = document.getElementById("latitude");
   const fourSquareId = document.getElementById("fourSquareId");
+
+  // Assign FourSquare URL variables
+  const urlRoot = "https://api.foursquare.com/v2/venues/"
+  const urlRootSearch = `${urlRoot}search?`;
+  const location = "bali";
+  const credentials = "&client_id=FUTTDSBGYDES4JISQRJTGHSIO4ELHQZQXXIO0FQWERW3EIMO&client_secret=XIU3I03M3Z42HDCIXWUTNEPK1YHJPYRQU1LEJBA50FJO1I4V"
+  const versionDate = "&v=20190703";
+  const limit = "&limit=10";
+  const foodCategoryId = "4d4b7105d754a06374d81259"
+  const barCategoryId = "4bf58dd8d48988d116941735"
+  const hotelCategoryId = "4bf58dd8d48988d1fa931735"
+  const corporateCoffeeShopId = "5665c7b9498e7d8a4f2c0f06"
+  const coworkingSpaceId = "4bf58dd8d48988d174941735"
+  const categoryIds = `${foodCategoryId},${barCategoryId},${hotelCategoryId},${corporateCoffeeShopId},${coworkingSpaceId}`
+
+
+  // Begin Search Query
   let searchQuery = "";
 
+  // Add keydown event listener
   searchBar.addEventListener('keydown', (event) => {
     console.log (event);
+    // Kill previous reults on every keydown event
+    dropdown.innerHTML = "";
+
+    // Capture keydown character
     const character = event.key;
-    if (character === "Backspace") {
+    // Remove last character from search query if character is backspace
+    if (character === "Backspace"){
       searchQuery = searchQuery.slice(0, -1);
       if (searchQuery.length > 0)
         myDropdown.classList.remove("invisible");
@@ -22,27 +46,15 @@ const venueAutofill = () => {
       searchQuery += event.key;
       myDropdown.classList.remove("invisible");
     }
+    // Log the search query
     console.log(searchQuery);
-    dropdown.innerHTML = "";
 
-    const URLRoot = "https://api.foursquare.com/v2/venues/search?";
-    const location = "bali";
-    const credentials = "REDACTED - MOVED TO .ENV"
-    const versionDate = "&v=20190703";
-    const limit = "&limit=10";
-    const foodCategoryId = "4d4b7105d754a06374d81259"
-    const barCategoryId = "4bf58dd8d48988d116941735"
-    const hotelCategoryId = "4bf58dd8d48988d1fa931735"
-    const corporateCoffeeShopId = "5665c7b9498e7d8a4f2c0f06"
-    const coworkingSpaceId = "4bf58dd8d48988d174941735"
-    const categoryIds = `${foodCategoryId},${barCategoryId},${hotelCategoryId},${corporateCoffeeShopId},${coworkingSpaceId}`
-    console.log(categoryIds);
-
-    fetch(`${URLRoot}near=${location}&query=${searchQuery}&categoryId=${categoryIds}${limit}${credentials}${versionDate}`)
+    // Call the FourSquare API
+    fetch(`${urlRootSearch}near=${location}&query=${searchQuery}&categoryId=${categoryIds}${limit}${credentials}${versionDate}`)
       .then(response => response.json())
       .then((data) => {
         console.log(data);
-        // generate list
+        // Generate html list
         data.response.venues.forEach((venue) => {
           dropdown.insertAdjacentHTML("afterbegin", `<li class="list-group-item" id=${venue.id}>${venue.name}</li>`);
         });
@@ -61,19 +73,20 @@ const venueAutofill = () => {
         // list item selection
         listItems.forEach((listItem) => {
           listItem.addEventListener("click", (click) => {
+            // Get the FourSquare id from the <li>
             let clickedListItemId = listItem.id;
-            console.log(`clickedListItemId is ${clickedListItemId}`);
+            // Reassign searchQuery to be the name of the selected venue
             searchQuery = listItem.innerHTML;
+            // Make the value attribute of venueNameInput field to be the same as the venue
             venueNameInput.value = searchQuery;
+            // Kill the venue results list
             myDropdown.classList.add("invisible");
+            // Check the clickedListItemId against the venue results to rematch object
             data.response.venues.forEach((venue) => {
               if (clickedListItemId == venue.id){
-                console.log(`i found a match for ${clickedListItemId}`);
-                console.log(`i matched ${venue.name}`);
-                console.log(`lat is ${venue.location.lat}`);
-                console.log(`lng is ${venue.location.lng}`);
-                console.log(`formatted address is ${venue.location.formattedAddress}`);
-                console.log(`foursquare_id is ${venue.id}`);
+                // Console log the matched venue
+                console.log(venue);
+                // Assign other form values with object data for venue
                 venue.location.formattedAddress.forEach((line) => {
                   address.value += `${line}, `;
                 });
@@ -82,6 +95,18 @@ const venueAutofill = () => {
                 longitude.value = venue.location.lat;
                 latitude.value = venue.location.lng;
                 fourSquareId.value = venue.id;
+                // findVenueOpeningTimes(venue.id)
+
+
+
+                // Fetch opening hours information with venue specific search
+                // fetch(`${urlRoot}${venue.id}/hours?${credentials}${versionDate}`)
+                //   .then(response => response.json())
+                //   .then((data) => {
+                //     console.log(data);
+                //     console.log(data.response.hours.timeframes[0]);
+
+                // });
               }
             });
           });
