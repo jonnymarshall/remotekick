4fa8958662cea33c35f0b602f3d923a7560f39a3
Jonny Marshall <jonnymarshall5@gmail.com>
2019-07-05 17:34:35 +0800

    algolia places api working to a basic degree

--- app/javascript/components/algolia_places.js
+++ app/javascript/components/algolia_places.js
@@ -1,5 +1,12 @@
 const searchAlgoliaPlaces = (event) => {
+  console.log("hello from algolia_places.js");
 
+  const input = document.getElementById("search");
+  input.addEventListener("keyup", (event) => {
+    console.log(event);
+  });
+
+const fetchData = () => {
   fetch("https://places-dsn.algolia.net/1/places/query", {
     method: "POST",
     body: JSON.stringify({ query: event.currentTarget.value })
@@ -8,17 +15,44 @@ const searchAlgoliaPlaces = (event) => {
     .then((data) => {
       console.log(data.hits); // Look at local_names.default
     });
+}
+
 };
 
-const logHello = () => {
-  console.log("hello");
+const testAPI = () => {
+  console.log("hello from algolia_places.js");
+  const input = document.getElementById("search");
+  const testList = document.getElementById("test-list");
+
+  const searchAlgoliaPlaces = (event) => {
+    fetch("https://places-dsn.algolia.net/1/places/query", {
+      method: "POST",
+      body: JSON.stringify({ query: event.currentTarget.value })
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data.hits.slice(0, 5));
+        // kill existing list if it exists
+        if (typeof testList !== 'undefined'){
+          testList.innerHTML = "";
+        }
+        // generate new list of first 5 results
+        data.hits.slice(0, 5).forEach((location) => {
+          if (typeof location.administrative !== 'undefined'){
+            if (typeof location.administrative[0] !== 'undefined'){
+              console.log(location.administrative[0]);
+              testList.insertAdjacentHTML("afterbegin", `<li>${location.administrative[0]}, ${location.country.default}</li>`);
+            }
+          }
+          let listItems = document.querySelectorAll("li");
+        });
+      });
+  };
+
+  input.addEventListener("keyup", searchAlgoliaPlaces);
 }
 
-const input = document.querySelector("#search");
 
-const doSomething = () => {
-  console.log("hello from algolia_places");
-  input.addEventListener("keyup", logHello);
-}
 
-export { doSomething }
+// export { searchAlgoliaPlaces }
+export { testAPI }
