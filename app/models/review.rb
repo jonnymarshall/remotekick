a1d35fff07f6252d16ef089794e26e1b79ba52a1
Jonny Marshall <jonnymarshall5@gmail.com>
2019-11-15 16:02:20 +1100

    review/new page revamp complete. carousel to be added

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :coffee_shop
   has_many :review_photos, dependent: :destroy
-  # validates_numericality_of :rating, greater_than_or_equal_to: 0, less_than_or_equal_to: 4
+  validates_numericality_of :rating, greater_than_or_equal_to: 0, less_than_or_equal_to: 4
   validates_numericality_of :plug_sockets, greater_than_or_equal_to: 0, less_than_or_equal_to: 2, allow_blank: true
   validates_numericality_of :comfort, greater_than_or_equal_to: 0, less_than_or_equal_to: 2, allow_blank: true
   validates_numericality_of :busyness, greater_than_or_equal_to: 0, less_than_or_equal_to: 2, allow_blank: true
