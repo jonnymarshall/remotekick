2432cb7e1afd6d88e5fb94972c041d06204fa38e
Jonny Marshall <jonnymarshall5@gmail.com>
2020-02-19 17:59:16 +1100

    refactored recalculate_values code and removed activerecord iteration causing inefficiencies

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,6 @@ class ReviewsController < ApplicationController
   before_action :assign_coffee_shop, only: [:show, :create, :new, :edit, :update, :destroy]
 
   def new
-    # @coffee_shop = CoffeeShop.find(coffee_shop_params[:coffee_shop_id])
     @review = current_user.reviews.new
     @descriptives = ["wonderful", "fabulous", "superb", "amazing", "stupendous", "phenomenal"]
     @review_photo = ReviewPhoto.new
@@ -15,37 +14,11 @@ class ReviewsController < ApplicationController
       @review_photo = @review.review_photos.new(photo: review_photo_params[:review_photo][:photo])
       @review_photo.save!
     end
-    # byebug
     @review.save!
     redirect_to coffee_shop_path(@coffee_shop)
-#       respond_to do |format|
-#         format.js
-#       end
-#     else
-#       render :new
-#     end
   end
 
   private
-
-  # def review_and_review_photo_params
-  #   params.require(:review).permit(
-  #     review: {
-  #       :content,
-  #       :rating,
-  #       :plug_sockets,
-  #       :comfort,
-  #       :busyness,
-  #       :upload_speed,
-  #       :download_speed,
-  #       :ping,
-  #       review_photo: [{
-  #         :photo
-  #         }
-  #       ]
-  #     }
-  #   )
-  # end
 
   def review_params
     params.require(:review).permit(
@@ -75,30 +48,3 @@ class ReviewsController < ApplicationController
     params.permit(:coffee_shop_id)
   end
 end
-
-# class ReviewsController < ApplicationController
-#   def create
-#     @review = Review.new(review_params)
-#     @review.booking_id = booking_params[:booking_id].to_i
-
-#     @booking = Booking.find(booking_params[:booking_id])
-#     render :new if @booking.user != current_user
-#     if @review.save
-#       respond_to do |format|
-#         format.js
-#       end
-#     else
-#       render :new
-#     end
-#   end
-
-#   private
-
-#   def review_params
-#     params.require(:review).permit(:content, :item_rating, :lender_rating)
-#   end
-
-#   def booking_params
-#     params.permit(:booking_id)
-#   end
-# end
