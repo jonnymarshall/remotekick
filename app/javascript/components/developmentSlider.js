1ab277b308b41d4c632cbdd1c79b867b5940910d
Jonny Marshall <jonnymarshall5@gmail.com>
2020-03-25 19:27:06 -0400

    wifi slider working and on filters page. in process of fixing mapbox issue

--- app/javascript/components/developmentSlider.js
+++ app/javascript/components/developmentSlider.js
@@ -1,28 +1,47 @@
 const developmentSlider = () => {
+  console.log(`hello from development slider`)
 
 	var parent = document.querySelector("#rangeSlider");
 	if(!parent) return;
 
 	var
-	rangeS = parent.querySelectorAll("input[type=range]"),
+  // rangeS = parent.querySelectorAll("input[type=range]")
+  range = parent.querySelector("input[type=range]"),
 		numberS = parent.querySelectorAll("input[type=number]");
 
-	rangeS.forEach(function(el) {
-		el.oninput = function() {
-			var slide1 = parseFloat(rangeS[0].value),
-				slide2 = parseFloat(rangeS[1].value);
+	// rangeS.forEach(function(el) {
+	// 	el.oninput = function() {
+	// 		var slide1 = parseFloat(rangeS[0].value),
+	// 			slide2 = parseFloat(rangeS[1].value);
 
-			if (slide1 > slide2) {
-				[slide1, slide2] = [slide2, slide1];
-				// var tmp = slide2;
-				// slide2 = slide1;
-				// slide1 = tmp;
-			}
+	// 		if (slide1 > slide2) {
+	// 			[slide1, slide2] = [slide2, slide1];
+	// 			// var tmp = slide2;
+	// 			// slide2 = slide1;
+	// 			// slide1 = tmp;
+	// 		}
 
-			numberS[0].value = slide1;
-			numberS[1].value = slide2;
-		}
-	});
+	// 		numberS[0].value = slide1;
+	// 		numberS[1].value = slide2;
+	// 	}
+  // });
+  
+  // range.forEach(function(el) {
+  range.oninput = function() {
+    var slide = parseFloat(range.value);
+    // console.log(range.value)
+      // slide2 = parseFloat(rangeS[1].value);
+
+    // if (slide1 > slide2) {
+    //   [slide1, slide2] = [slide2, slide1];
+    //   // var tmp = slide2;
+    //   // slide2 = slide1;
+    //   // slide1 = tmp;
+    // }
+    numberS[0].value = slide;
+    // numberS[1].value = slide2;
+  }
+	// });
 
 	numberS.forEach(function(el) {
 		el.oninput = function() {
