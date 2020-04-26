83dcae908c1966b2ebe609ff637a1dd9b42dd2f9
Jonny Marshall <jonnymarshall5@gmail.com>
2020-04-26 22:31:41 +0100

    initial tab JS working - frontend only

---
+++ app/javascript/packs/controllers/tags_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+
+  controllerName = "tags_controller"
+
+  static targets = ["tag"]
+
+  connect() {
+    console.log(`${this.controllerName} connected.`)
+    console.log(this.tagTargets)
+  }
+
+  disconnect() {
+    console.log(`${this.controllerName} disconnected.`)
+  }
+
+  activateTag(e) {
+    const tagIsActive = () => {
+      return e.target.classList.contains("is-primary")
+    }
+    if (!tagIsActive()) {
+      e.target.insertAdjacentHTML("beforeend", '<button class="delete is-small" data-action="tags#deactivateTag"></button>');
+    } else {
+      e.target.removeChild(e.target.lastElementChild);
+    }
+    e.target.classList.toggle("is-primary")
+  }
+
+  deactivateTag(e) {
+    console.log('did ya remove?')
+    e.target.parentElement.classList.remove("is-primary")
+    e.target.remove()
+  }
+
+}
