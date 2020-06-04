ddaf0ced2c5f965764c5062fbfa3b324c3e2f356
Jonny Marshall <jonnymarshall5@gmail.com>
2020-06-04 17:34:37 +0100

    email front end config set up with premailer-rails

--- spec/mailers/previews/venue_mailer_preview.rb
+++ spec/mailers/previews/venue_mailer_preview.rb
@@ -1,7 +1,7 @@
 class VenueMailerPreview < ActionMailer::Preview
   def new_venue_listed
-    user = FactoryBot.build(:user)
-    venue = FactoryBot.build(:venue)
+    user = FactoryBot.build_stubbed(:user)
+    venue = FactoryBot.build_stubbed(:venue)
     VenueMailer.new_venue_listed(user: user, venue: venue)
   end
 end
