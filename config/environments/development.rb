33488d28f3fe02ae45ad94870fc25ca80e48ee90
Jonny Marshall <jonnymarshall5@gmail.com>
2020-06-25 19:34:00 +0100

    moved actionmailer settings to environment.rb to send emails in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,27 +64,12 @@ Rails.application.configure do
   # Ngrok
   config.hosts << "248c75ce.ngrok.io"
 
-  # ActionMailer
-  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-
   config.active_record.dump_schema_after_migration = false
 
+  # ActionMailer
+  
   # config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  # config.action_mailer.delivery_method = :test
   config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
-  config.action_mailer.show_previews = true
-
-    config.action_mailer.smtp_settings = {
-      :address              => "mail.privateemail.com",
-      :port                 => 587,
-      :user_name            => ENV['REMOTEKICK_EMAIL_USERNAME'],
-      :password             => ENV['REMOTEKICK_EMAIL_PASSWORD'],
-      :authentication       => :plain, #:login, :cram_md5
-      :enable_starttls_auto => true
-    }
 
     # config.action_mailer.smtp_settings = {
     #   :address              => "smtp.gmail.com",
