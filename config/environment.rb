f8039633da1ebad4c80dcf1e783bee80ac499a95
Jonny Marshall <jonnymarshall5@gmail.com>
2020-06-25 20:22:38 +0100

    moved smtp settings to production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -7,23 +7,3 @@ Rails.application.initialize!
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   html_tag.html_safe
 end
-
-Rails.application.configure do
-
-  # ActionMailer
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
-  config.action_mailer.show_previews = true
-  config.action_mailer.default_url_options = { :host => ENV['DOMAIN'] }
-
-  config.action_mailer.smtp_settings = {
-    :address              => "mail.privateemail.com",
-    :port                 => 587,
-    :user_name            => ENV['REMOTEKICK_EMAIL_USERNAME'],
-    :password             => ENV['REMOTEKICK_EMAIL_PASSWORD'],
-    :authentication       => :plain,
-    :enable_starttls_auto => true
-  }
-end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,23 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  # Do not dump schema after migrations.
+  Rails.application.configure do
+
+    # ActionMailer
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
+    config.action_mailer.show_previews = true
+    config.action_mailer.default_url_options = { :host => ENV['DOMAIN'] }
+  
+    config.action_mailer.smtp_settings = {
+      :address              => "mail.privateemail.com",
+      :port                 => 587,
+      :user_name            => ENV['REMOTEKICK_EMAIL_USERNAME'],
+      :password             => ENV['REMOTEKICK_EMAIL_PASSWORD'],
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+    }
+  end
 end
