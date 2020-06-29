5de914b80cae243dc1489d46fd3e0e64d06b055c
Jonny Marshall <jonnymarshall5@gmail.com>
2020-06-29 13:52:42 +0100

    added first name and last name to devise form for user authentication. Prevented redirect to homepage on sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,21 @@ class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :null_session
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, :unless => :devise_controller?
+
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+  
+  
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
+
+  def store_current_location
+    store_location_for(:user, request.url)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable #, :confirmable
 
   has_many :venues
   has_many :reviews
 
   validates :email, presence: true, uniqueness: true
-  # validates :first_name, presence: true
-  # validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def owned_venues
     Venues.where(owner: self)
