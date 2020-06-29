1a17bc9de57ea6faa010407ad5c15aa617769395
Jonny Marshall <jonnymarshall5@gmail.com>
2020-06-29 20:04:15 +0100

    changed default from email for devise emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,13 +5,11 @@ class UserMailer < Devise::Mailer
   default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
   # If there is an object in your application that returns a contact email, you can use it as follows
   # Note that Devise passes a Devise::Mailer object to your proc, hence the parameter throwaway (*).
-  default from: "->(*) { Class.instance.email_address }"
+  # default from: "->(*) { Class.instance.email_address }"
   layout 'mailer'
 
   def confirmation_instructions(record, token, opts={})
     headers["Custom-header"] = "Bar"
-    opts[:from] = 'my_custom_from@domain.com'
-    opts[:reply_to] = 'my_custom_from@domain.com'
     @root_path = root_path
     @fist_name = record[:first_name]
     super
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'donotreply@remotekick.com'
+  config.mailer_sender = 'remotekick <team@remotekick.com>'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'UserMailer'
