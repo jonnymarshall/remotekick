288942907a676fe8b4c3c2e71f6998e33962468c
Jonny Marshall <jonnymarshall5@gmail.com>
2020-07-01 17:38:18 +0100

    removed byebug from mailer

--- app/mailers/venue_mailer.rb
+++ app/mailers/venue_mailer.rb
@@ -23,7 +23,6 @@ class VenueMailer < ApplicationMailer
       @root_path = "http://#{ENV['LOCAL_DOMAIN']}"
     end
     
-    byebug
     @venue_url = URI.join(@root_path, venue_path(venue)).to_s
     @edit_venue_url = URI.join(@root_path, edit_venue_path(venue)).to_s
     @new_venue_review_path = URI.join(@root_path, new_venue_review_path(venue)).to_s
