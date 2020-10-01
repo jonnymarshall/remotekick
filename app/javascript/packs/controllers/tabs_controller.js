c86726ab339859450844a3796f471530d63168fb
Jonny Marshall <jonny@Jonnys-MBP.cust.communityfibre.co.uk>
2020-10-01 17:56:17 +0100

    show page tabs done. map and reviews each have own tab

--- app/javascript/packs/controllers/tabs_controller.js
+++ app/javascript/packs/controllers/tabs_controller.js
@@ -3,12 +3,12 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   controllerName = "tabs_controller";
 
-  static targets = ["tab"];
+  static targets = ["tab", "contentSection"];
 
   connect() {
     console.log(`${this.controllerName} connected.`);
-    console.log(this.tabTargets[0]);
     this.tabTargets[0].classList.add("is-active");
+    // this.contentSectionTargets[0].classList.remove("is-hidden");
   }
 
   disconnect() {
@@ -19,5 +19,15 @@ export default class extends Controller {
     this.tabTargets.forEach((tab) => tab.classList.remove("is-active"));
     const tabElement = e.target.parentElement;
     tabElement.classList.add("is-active");
+    this.toggleTabContent(tabElement);
+  }
+
+  toggleTabContent(tabElement) {
+    this.contentSectionTargets.forEach((sectionTarget) => {
+      sectionTarget.classList.add("is-hidden");
+      if (sectionTarget.dataset.sectionId === tabElement.dataset.tabId) {
+        sectionTarget.classList.remove("is-hidden");
+      }
+    });
   }
 }
