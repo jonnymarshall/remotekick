37d3100efdf21a06be98691e6d5896fe5b284004
Jonny Marshall <jonny@Jonnys-MacBook-Pro.local>
2020-10-05 17:26:44 +0100

    fixed test

--- spec/features/venue_page_spec.rb
+++ spec/features/venue_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Show Venue' do
   let(:u) { create(:user) }
   let(:ven) { create(:venue, users: [u]) }
-  
+  let!(:leeds_address) { FactoryBot.create(:address, venue: ven) }
 
   describe 'showing a new venue' do
     it 'shows the venue' do
