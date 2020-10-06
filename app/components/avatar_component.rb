a889786e828ca85ec3e0ccfa71d84bf636b5db44
Jonny Marshall <jonny@Jonnys-MacBook-Pro.local>
2020-10-06 16:26:54 +0100

    dynamic avatar component created

--- app/components/avatar_component.rb
+++ app/components/avatar_component.rb
@@ -2,22 +2,22 @@ class AvatarComponent < ViewComponent::Base
 
   FALLBACK = "https://api.adorable.io/avatars/285/abott@adorable.png"
 
-  SIZE_OPTIONS = {
-    "small" => {width: 64, height: 64},
-    "large" => {width: 220, height: 220}
-  }
-
-  def initialize(user:, size:, rounded: false)
+  def initialize(user:, media_object: false)
     @user = user
-    @avatar_url = "https://api.adorable.io/avatars/285/abott@adorable.png"
-    @size = size
-    @rounded = rounded
+    @media_object = media_object
+    @rounded = ""
   end
 
   def before_render
     @avatar_url = @user.avatar.attached? ? helpers.url_for(@user.avatar.attachment) : FALLBACK
-    @width = SIZE_OPTIONS.fetch(@size)[:width]
-    @height = SIZE_OPTIONS.fetch(@size)[:height]
-    @rounded = @rounded ? "is-rounded" : ""
+    assign_size if @media_object
+    @media_object ? @rounded = "is-rounded" : @rounded = ""
+  end
+
+  private
+
+  def assign_size
+    @height = "64"
+    @width = "64"
   end
 end
